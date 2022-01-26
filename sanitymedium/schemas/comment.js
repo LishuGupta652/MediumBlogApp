900492ed3c36aabddcaecc6dbabdd997ff87e25f
lishu <lishugupta652@gmail.com>
2022-01-26 20:17:52 +0530

    sanity

---
+++ sanitymedium/schemas/comment.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'comment',
+  type: 'comment',
+  title: 'Comment',
+  fields: [
+    {
+      name: 'name',
+      type: 'stirng',
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'text',
+      type: 'reference',
+      to: [{ type: 'post' }],
+    },
+  ],
+}
