dd52c8770be0f5d3d34fd29a62e870e439167f0b
lishu <lishugupta652@gmail.com>
2022-01-26 17:02:50 +0530

    sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       </div>
 
       {/* posts */}
-      
+       
     </div>
   )
 }
---
+++ sanity.js
@@ -0,0 +1,13 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUserHook,
+  createClient,
+} from 'next-sanity'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2022-01-26',
+
+  useCdn: (process.env.NODE_ENV = 'production'),
+}
