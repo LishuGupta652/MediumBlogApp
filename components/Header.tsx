949f839bc8346a6b7ac197094f19c33b0b6dc607
lishu <lishugupta652@gmail.com>
2022-01-26 16:18:32 +0530

    terminalUpdates

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+
+      <header className='flex justify-between p-5 max-w-7xl mx-auto  items-center'>
+      <div className='flex items-center space-x-5'>
+          <Link href="/">
+              <img className="w-44 object-contain cursor-pointer" src="logo.png" alt="logo" /> 
+          </Link>
+          <div className='hidden md:inline-flex item-center space-x-5'>
+              <h3>About</h3>
+              <h3>Contact</h3>
+              <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
+          </div>
+      </div>
+      <div className='flex items-center space-x-5 text-green-600'>
+        <h3>Sign In</h3>
+        <h3 className='border px-4 py-1 rounded-full border-green-600'>Get Started</h3>
+      </div>
+  </header>
+  )
+};
+
+export default Header;
