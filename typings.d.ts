9e3b3965fd933f42f7a65dafa07fc6258361ad32
lishu <lishugupta652@gmail.com>
2022-01-26 20:35:27 +0530

    sanity

--- typings.d.ts
+++ typings.d.ts
@@ -17,3 +17,19 @@ export interface Post {
     };
     body: [object];
 }
+
+export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string,
+    name: string,
+    post: {
+        _ref: string;
+        _type: string;
+    }
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+}
