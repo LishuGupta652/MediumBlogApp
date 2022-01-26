52121ac9b326889f53800c133fcf0b3d066b0b4c
lishu <lishugupta652@gmail.com>
2022-01-26 19:38:20 +0530

    sanity

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -25,7 +25,7 @@ const Post = ({post} : Props) => {
             <p className="font-extralight text-sm">Blog post by <a href="https://lishu.ml"> <span className="text-green-600"> {post.author.name} </span></a> -Published at  {new Date(post._createdAt).toLocaleString()}</p>
           </div>
 
-          <div>
+          <div className="mt-10 ">
               <PortableText 
                  className=""
                 dataset="production"
@@ -43,6 +43,29 @@ const Post = ({post} : Props) => {
           </div>
           
       </article>
+
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500 "/>
+
+      <form className="flex flex-col my-10 p-5 max-w-2xl mx-auto mb-10">
+
+          <h3 className="text-sm text-yellow-500">Enjoy this article</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr  className="py-3 mt-2"/>
+          <label className="block mb-5 " htmlFor="">
+              <span className="text-gray-700 ">Name</span>
+              <input className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500  focus:ring outline-none " type="text" placeholder="Lishu Gupta" />
+          </label>
+          <label className="block mb-5 " htmlFor="">
+              <span className="text-gray-700 ">Email</span>
+              <input className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring  " type="email" placeholder="test@mail.com" />
+          </label>
+          <label className="block mb-5 " htmlFor="">
+              <span className="text-gray-700 ">Comment</span>
+              <textarea className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none" placeholder="Comment" rows={8} />
+          </label>
+          
+      </form>
   </main>
 };
 
