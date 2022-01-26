a8f8eac28791aede042dd4c563ea3b06841f27a9
lishu <lishugupta652@gmail.com>
2022-01-26 18:38:13 +0530

    sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from '../components/Header'
 import {sanityClient, urlFor } from "../sanity"
 import {Post} from "../typings"
@@ -6,7 +7,7 @@ interface Props {
   posts: [Post];
 }
 
-export default function Home({posts}: any) {
+export default function Home({posts}: Props) {
   console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
@@ -29,9 +30,28 @@ export default function Home({posts}: any) {
 
         <img className="hidden md:inline-flex h-32 lg:h-min " src="bannerlogo.png" alt="" />
       </div>
+   {/* posts */}
+       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+         {
+           posts.map(post => (
 
-      {/* posts */}
-       
+             <Link key={post._id} href={`/post/${post.slug.current}`}>
+                <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+                  <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={ urlFor(post.mainImage).url()!} alt={post.description} />
+                  <div className='flex justify-between p-5 bg-white '>
+                    <div>
+                      <p className='text-lg font-bold'>{post.title}</p>
+                      <p className='text-xs'>{post.description} by {post.author.name}</p>
+                    </div>
+
+                    <img  className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt={post.author.name} />
+                  </div>
+                </div>
+             </Link>
+           )
+           )
+         }
+       </div>
     </div>
   )
 }
